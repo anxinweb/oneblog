e84ae91ab20c699b30ae21c42b23720bfa93284d
fangs <45865656+anxinweb@users.noreply.github.com>
2021-12-29 04:31:17 +0800

    Update nuxt.config.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -6,13 +6,13 @@ export default defineNuxtConfig({
         'nuxt-windicss',
     ],
     publicRuntimeConfig: {
-        WEBSITE_NAME: 'Arect\'s blog'
+        WEBSITE_NAME: 'xin's blog'
     },
     privateRuntimeConfig: {
-        CLIENT_ID: process.env.CLIENT_ID,
-        CLIENT_SECRET: process.env.CLIENT_SECRET,
+        CLIENT_ID: 14454d53-0c1d-4ce6-ad80-06f9b386970e,
+        CLIENT_SECRET: zDd7Q~risFWZDoj3ZTFYjdMuafWqUu.uiqLYZ,
         REDIRECT_URI: 'http://localhost:3000',
-        REFRESH_TOKEN: process.env.REFRESH_TOKEN,
+        REFRESH_TOKEN: M.R3_BAY.c33a0914-ec48-4670-9252-ea618319cd30,
         ROOT_PATH: '/Blog',
         ONEDRIVE_URI: 'https://graph.microsoft.com/v1.0/me/drive',
         OAUTH_URL: 'https://login.microsoftonline.com/common/oauth2/v2.0/token'
